803312faf97a53a745e5da2714c30280f2176532
Malcovys <malcovys22.aps@gmail.com>
2023-08-06 10:12:47 +0300

    update data base model

--- API/library.sql
+++ API/library.sql
@@ -1,75 +1,86 @@
 CREATE TABLE `auteur` (
-  `num_auteur` integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `nom_auteur` varchar(20) NOT NULL
-);
-
-CREATE TABLE `livre` (
-  `num_livre` integer PRIMARY KEY NOT NULL,
-  `titre_livre` varchar(30) NOT NULL,
-  `categorie_livre` varchar(15) NOT NULL,
-  `quantite_livre` integer NOT NULL
+  `num_auteur` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nom_auteur` VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE `ouvrage` (
-  `num_livre` integer PRIMARY KEY NOT NULL,
-  `num_auteur` integer NOT NULL
+  `num_ouvrage` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `isbn` VARCHAR(20) NOT NULL,
+  `num_auteur` INT NOT NULL
+);
+
+CREATE TABLE `livre` (
+  `isbn` VARCHAR(20) PRIMARY KEY NOT NULL,
+  `tire` VARCHAR(30) NOT NULL,
+  `num_maison` INT NOT NULL,
+  `categorie` VARCHAR(15) NOT NULL,
+  `quantite` INT NOT NULL,
+  `date_edition` DATE NOT NULL
+);
+
+CREATE TABLE `maison` (
+  `num_maison` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nom_maison` VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE `exemplaire` (
-  `isbn` varchar(20) PRIMARY KEY NOT NULL,
-  `num_livre` integer NOT NULL,
-  `date_enregistrement` date NOT NULL,
-  `statut_livre` boolean DEFAULT 1 NOT NULL
+  `num_exemplaire` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `isbn` VARCHAR(20) NOT NULL,
+  `date_enregistrement` DATE NOT NULL,
+  `statut_exemplaire` BOOLEAN NOT NULL DEFAULT 1
 );
 
 CREATE TABLE `adherent` (
-  `id_adherent` integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `nom_adherent` varchar(50) NOT NULL,
-  `mail_adherent` varchar(60) NOT NULL,
-  `adresse_adherent` varchar(50) NOT NULL,
-  `date_admission` date NOT NULL,
-  `validite_adherent` integer NOT NULL,
-  `mpd` varchar(255) NOT NULL,
-  `statut_adherent` boolean DEFAULT 1 NOT NULL,
-  `type_compte` varchar(3) NOT NULL
-);
-
-CREATE TABLE `staff` (
-  `id_staff` integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `id_adherent` integer NOT NULL
+  `id_adherent` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(50) NOT NULL,
+  `prenom` VARCHAR(50) NOT NULL,
+  `mail` VARCHAR(200) NOT NULL,
+  `adresse` VARCHAR(50) NOT NULL,
+  `date_admission` DATE NOT NULL,
+  `abonement` INT,
+  `type_compte` CHAR(3) NOT NULL DEFAULT 'ADR',
+  `mdp` VARCHAR(255) NOT NULL,
+  `statut_adherent` BOOLEAN NOT NULL DEFAULT 1,
+  `token` VARCHAR(255)
 );
 
 CREATE TABLE `emprunt` (
-  `num_emprunt` integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `date_emprunt` date NOT NULL,
-  `date_echeance` date NOT NULL,
-  `date_retour` date,
-  `isbn` varchar(20) NOT NULL,
-  `id_adherent` integer NOT NULL,
-  `id_staff` integer NOT NULL
+  `num_emprunt` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `id_adherent` INT NOT NULL,
+  `num_exemplaire` INT NOT NULL,
+  `date_emprunt` DATE NOT NULL,
+  `date_echeance` DATE NOT NULL,
+  `date_retour` DATE
 );
 
-CREATE TABLE `sanction` (
-  `num_sanction` integer PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `id_adherent` integer NOT NULL,
-  `type_sanction` varchar(2) NOT NULL,
-  `sanction` varchar(4) NOT NULL
+CREATE TABLE `penalite` (
+  `num_sanction` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `id_adherent` INT NOT NULL,
+  `type_sanction` Char(2) NOT NULL,
+  `sanction` VARCHAR(5) NOT NULL
 );
 
-CREATE TABLE `commentaire` (
-  `id_commentaire` varchar(255) PRIMARY KEY NOT NULL,
-  `id_adherent` integer NOT NULL,
-  `num_livre` integer NOT NULL,
-  `avis` varchar(255) NOT NULL
+CREATE TABLE `avis` (
+  `num_avis` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `isbn` VARCHAR(20) NOT NULL,
+  `id_adherent` INT NOT NULL,
+  `commentaire` VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE `ouvrage` ADD FOREIGN KEY (`num_livre`) REFERENCES `livre` (`num_livre`);
+ALTER TABLE `penalite` ADD FOREIGN KEY (`id_adherent`) REFERENCES `adherent` (`id_adherent`);
+
+ALTER TABLE `exemplaire` ADD FOREIGN KEY (`isbn`) REFERENCES `livre` (`isbn`);
+
+ALTER TABLE `avis` ADD FOREIGN KEY (`id_adherent`) REFERENCES `adherent` (`id_adherent`);
+
+ALTER TABLE `avis` ADD FOREIGN KEY (`isbn`) REFERENCES `livre` (`isbn`);
+
+ALTER TABLE `livre` ADD FOREIGN KEY (`num_maison`) REFERENCES `maison` (`num_maison`);
+
+ALTER TABLE `ouvrage` ADD FOREIGN KEY (`isbn`) REFERENCES `livre` (`isbn`);
+
 ALTER TABLE `ouvrage` ADD FOREIGN KEY (`num_auteur`) REFERENCES `auteur` (`num_auteur`);
-ALTER TABLE `exemplaire` ADD FOREIGN KEY (`num_livre`) REFERENCES `livre` (`num_livre`);
-ALTER TABLE `staff` ADD FOREIGN KEY (`id_adherent`) REFERENCES `adherent` (`id_adherent`);
-ALTER TABLE `emprunt` ADD FOREIGN KEY (`isbn`) REFERENCES `exemplaire` (`isbn`);
+
 ALTER TABLE `emprunt` ADD FOREIGN KEY (`id_adherent`) REFERENCES `adherent` (`id_adherent`);
-ALTER TABLE `emprunt` ADD FOREIGN KEY (`id_staff`) REFERENCES `staff` (`id_staff`);
-ALTER TABLE `sanction` ADD FOREIGN KEY (`id_adherent`) REFERENCES `adherent` (`id_adherent`);
-ALTER TABLE `commentaire` ADD FOREIGN KEY (`id_adherent`) REFERENCES `adherent` (`id_adherent`);
-ALTER TABLE `commentaire` ADD FOREIGN KEY (`num_livre`) REFERENCES `livre` (`num_livre`);
+
+ALTER TABLE `emprunt` ADD FOREIGN KEY (`num_exemplaire`) REFERENCES `exemplaire` (`num_exemplaire`);

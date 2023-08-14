b2d142c310d242af3426b5ca1fd6b9a91e0b1f3c
anicet <anicet22.aps2@gmail.com>
2023-08-14 09:27:51 +0300

    LoanRegister page :  page d'enregistrement d'un prêt

--- front_end/library/src/App.vue
+++ front_end/library/src/App.vue
@@ -5,11 +5,12 @@ import SingIn from './components/SingIn.vue';
 import MembershipForm from './components/MembershipForm.vue';
 import AddBook from './components/AddBook.vue';
 import MembershipCard from './components/MembershipCard.vue';
+import LoanRegister from './components/LoanRegister.vue';
 
 </script>
 
 <template>
     <div>
-        <MembershipCard/>
+        <LoanRegister/>
     </div>
 </template>
